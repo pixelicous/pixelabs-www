11638b21c921409ce18eb55dbcc340121ef1d38f
Netz <pixelicous@users.noreply.github.com>
2020-05-26 03:22:07 +0300

    Cleanup&Comments (#3)
    * GITIGNORE update
    gallery_cat - update missing print to log info
    
    * make psychedelic gallery work, disable minimal bbox for now
    
    * gallery css cleanup
    
    * Add: baguetteBox.js
    Out random color fetch to function, don't repeat same color between fullscreen photos
    
    * more fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,27 +7,30 @@ function return_random_color() {
     'vintage',
   ];
 
+  // Make sure color changes between photo switch
   do {
     var duotoneColor = COLORS[Math.floor(Math.random() * COLORS.length)];
   }
-  while (duotoneColor == window.last_color)
-  window.last_color = duotoneColor;
+  while (duotoneColor == window.duo_last_color)
+
+  // Save duotone_last_color_globally
+  window.duo_last_color = duotoneColor;
+
   return duotoneColor
 }
 
-function colorful_full_screen(currentIndex) {
-
-  // var psychedelic_enabled = document.getElementById("pschedelic_gallery")
-
-  // if ( psychedelic_enabled.checked) {
-
+function colorful_full_screen() {
     var photo = document.querySelectorAll('[id^=fullsize_gallery]');
       photo[0].setAttribute('data-duotheme', return_random_color());
-    //}
+}
+
+function colorful_full_screen() {
+  var photo = document.querySelectorAll('[id^=fullsize_gallery]');
+  photo[0].setAttribute('data-duotheme', return_random_color());
 }
 
 jQuery(document).ready(function() {
-  "use strict"; // Start of use strict
+  "use strict"; 
 
   document.getElementById("pschedelic_gallery").onclick = function () {
     // query html galleries for this id, color effects will be enabled on those photos
