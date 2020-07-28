9878dd4073a499061e9d6d2f11249048dc86c0f4
pixelicous <pixi@pixelabs.net>
2020-07-28 16:22:31 +0300

    matplotlib more changes

--- tools/photo_functions.py
+++ tools/photo_functions.py
@@ -254,15 +254,15 @@ def export_photo_histogram(full_path, histo_path, overwrite):
 def export_nice_histogram(full_path, photo_histogram_file):
     image = io.imread(full_path)
 
+    histo = plot.hist(image.ravel(), bins = 256, color = 'grey', alpha = 0.3, histtype='step')
     histo = plot.hist(image[:, :, 0].ravel(), bins = 256, color = 'red', alpha = 0.5,histtype='step')
     histo = plot.hist(image[:, :, 1].ravel(), bins = 256, color = 'green', alpha = 0.5,histtype='step')
     histo = plot.hist(image[:, :, 2].ravel(), bins = 256, color = 'blue', alpha = 0.5,histtype='step')
     histo = plot.xlabel('Intensity')
     histo = plot.ylabel('Pixels #')
-    histo = plot.legend(['Total', 'Red Channel', 'Green Channel', 'Blue Channel'])
+    histo = plot.legend(['Total', 'Red Channel', 'Green Channel', 'Blue Channel'], loc='upper right')
     plot.savefig(photo_histogram_file, bbox_inches='tight', dpi=75, transparent=False)
     plot.close(photo_histogram_file)
-
 
 
 def export_photo_histogram_simple(full_path):
