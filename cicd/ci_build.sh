e9e65fcc4a219f45cf334fdf82bfc18885c1238e
pixelicous <pixi@pixelabs.net>
2020-07-31 16:11:24 +0300

    Try to clean jekyll cache

--- cicd/ci_build.sh
+++ cicd/ci_build.sh
@@ -3,3 +3,5 @@ set -e # halt script on error
 
 JEKYLL_ENV=production bundle exec jekyll build --destination site --trace
 bundle exec htmlproofer ./site --disable-external
+
+bundle exec jekyll clean
