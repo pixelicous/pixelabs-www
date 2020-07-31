dcb0fe4b41d3090d4053c54ea8ab0c90d2780d36
pixelicous <pixi@pixelabs.net>
2020-07-31 16:39:01 +0300

    litter clean execute fix

--- cicd/ci_build.sh
+++ cicd/ci_build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e # halt script on error
-python ./tools/little_cleaner.py
+python ./tools/litter_cleaner.py
 
 JEKYLL_ENV=production bundle exec jekyll build --destination site --trace
 bundle exec htmlproofer ./site --disable-external
